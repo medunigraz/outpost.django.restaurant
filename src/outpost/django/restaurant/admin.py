98de437bef571a302d932c761e7dabc11f210a56
Michael Fladischer <michael@fladi.at>
2024-05-06 23:06:26 +0200

    Add additional fields for manual restaurant admin.

--- src/outpost/django/restaurant/admin.py
+++ src/outpost/django/restaurant/admin.py
@@ -1,9 +1,17 @@
+from io import BytesIO
+
+import qrcode
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext as _
 from polymorphic.admin import (
     PolymorphicChildModelAdmin,
     PolymorphicChildModelFilter,
     PolymorphicParentModelAdmin,
 )
+from qrcode.image.svg import SvgPathImage
 
 from . import models
 
@@ -36,7 +44,32 @@ class XMLRestaurantAdmin(RestaurantChildAdmin):
 class ManualRestaurantAdmin(RestaurantChildAdmin):
     base_model = models.ManualRestaurant
     inlines = (MealInline, SpecialInline)
-    readonly_fields = ("secret",)
+    readonly_fields = ("secret", "link", "qrcode")
+
+    def change_view(self, request, *args, **kwargs):
+        self.request = request
+        return super().change_view(request, *args, **kwargs)
+
+    def link(self, obj):
+        url = self.request.build_absolute_uri(
+            reverse("restaurant:restaurant", kwargs={"secret": obj.secret})
+        )
+        return format_html("""<a href="{url}" target="_blank">{url}</a>""", url=url)
+
+    qrcode.short_description = _("Link")
+
+    def qrcode(self, obj):
+        b = BytesIO()
+        url = self.request.build_absolute_uri(
+            reverse("restaurant:restaurant", kwargs={"secret": obj.secret})
+        )
+        qr = qrcode.make(url, image_factory=SvgPathImage)
+        qr.save(b)
+        tag = b.getvalue().decode("utf-8")
+        b.close()
+        return mark_safe(tag)
+
+    qrcode.short_description = _("QR code")
 
 
 @admin.register(models.Restaurant)
